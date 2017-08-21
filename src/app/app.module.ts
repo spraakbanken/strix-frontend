c93bc4fe41c2eff14fda60525396c41942921d5f
Johan Roxendal <johan.roxendal@gu.se>
2017-08-21 09:46:35 +0000

    removed broken dep.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,7 +54,7 @@ import { SearchFilterComponent } from './search-filter/search-filter.component';
     IndocsearchComponent,
     HistogramComponent,
     AnnotationsSelectorComponent,
-    SearchFilterComponent
+    // SearchFilterComponent
   ],
   imports: [
     BrowserModule,
