351807340f72c0022893f482ac6b47ad60e3e419
Johan Roxendal <johan.roxendal@gu.se>
2018-01-26 10:37:23 +0100

    fixed broken await

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -8,7 +8,7 @@ describe('strix App', function() {
     page = new StrixPage();
   });
 
-  it('should show the relevant document', () => {
+  it('should show the relevant document', async () => {
     await browser.get('/?filters=W3siZmllbGQiOiJkYXRhdHlwIiwidmFsdWUiOiJodXZ1ZGRva3VtZW50In1d&documentID=25f95347-f194-4b1e-8737-9a4a41e86ae4&documentCorpus=rd-bet');  
     
     expect(element(by.css('.doc_header b')).getText()).toEqual('RD - Betänkande: Ny fastighetsmäklarlag (förnyad behandling)');
