557b431fa7550f2c908563b326f0ecb2c4c82ed1
Jonatan Uppström <jonatan.uppstrom@gu.se>
2018-02-01 11:11:16 +0100

    change of production backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: "https://ws.spraakbanken.gu.se/ws/strixlabb",
+  api: "https://ws.spraakbanken.gu.se/ws/strix",
   auth: "https://sp.spraakbanken.gu.se/auth"
 };
