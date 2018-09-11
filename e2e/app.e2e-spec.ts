0cef33c0ac68215824f06c31103003925da4833e
Arild Matsson <arild@klavaro.se>
2018-09-11 13:26:42 +0200

    A few less awaits

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,8 +2,8 @@ import { browser, by, element } from 'protractor';
 
 describe('strix App', function() {
 
-  it('should show the relevant document', async () => {
-    await browser.get('/?filters=W3siZmllbGQiOiJkYXRhdHlwIiwidmFsdWUiOiJodXZ1ZGRva3VtZW50In1d&documentID=25f95347-f194-4b1e-8737-9a4a41e86ae4&documentCorpus=rd-bet');  
+  it('should show the relevant document', () => {
+    browser.get('/?filters=W3siZmllbGQiOiJkYXRhdHlwIiwidmFsdWUiOiJodXZ1ZGRva3VtZW50In1d&documentID=25f95347-f194-4b1e-8737-9a4a41e86ae4&documentCorpus=rd-bet');
 
     expect(element(by.css('.doc_header b')).getText()).toEqual('RD - Betänkande: Ny fastighetsmäklarlag (förnyad behandling)');
   });
@@ -11,13 +11,10 @@ describe('strix App', function() {
   it('should filter when clicking a button in left sidebar', async () => {
     await browser.get('/');
 
-    let getNum = async () => {
-        let text = await element(by.css(".hits_header .num")).getText();
-        return Number(text.replace(' ', ''));
-    }
-    let num = await getNum();
-    await element(by.css(".aggregation_item")).click()
-    expect(getNum()).toBeLessThan(num);
+    const getNum = async () => element(by.css(".hits_header .num")).getText().then(text => Number(text.replace(' ', '')));
+    const num = await getNum();
 
+    element(by.css(".aggregation_item")).click()
+    expect(getNum()).toBeLessThan(num);
   });
 });
