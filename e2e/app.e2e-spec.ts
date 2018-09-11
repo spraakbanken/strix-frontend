51accf0405c4daacb0b1ad3bf92779a721e56a63
Arild Matsson <arild@klavaro.se>
2018-09-11 16:18:49 +0200

    Restored async/await, they are needed

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,19 +2,24 @@ import { browser, by, element, Key } from 'protractor';
 
 describe('strix App', function() {
 
-  it('should show the relevant document', () => {
-    browser.get('/?filters=W3siZmllbGQiOiJkYXRhdHlwIiwidmFsdWUiOiJodXZ1ZGRva3VtZW50In1d&documentID=25f95347-f194-4b1e-8737-9a4a41e86ae4&documentCorpus=rd-bet');
-
-    expect(element(by.css('.doc_header b')).getText()).toEqual('RD - Betänkande: Ny fastighetsmäklarlag (förnyad behandling)');
+  it('should show the relevant document', async () => {
+    await browser.get('?documentID=20d:0&documentCorpus=fragelistor');
+    const title = await element(by.css('.doc_header b')).getText();
+    // Corpus name not included in check, because browser locale is unknown.
+    await expect(title).toMatch('Knölpåkar');
   });
 
   it('should filter when clicking a button in left sidebar', async () => {
     await browser.get('/');
 
-    const getNum = async () => element(by.css(".hits_header .num")).getText().then(text => Number(text.replace(' ', '')));
+    async function getNum() {
+      let t = await element(by.css(".hits_header .num")).getText();
+      return Number(t.replace(/[^0-9]/g, ''));
+    }
+
     const num = await getNum();
 
-    element(by.css(".aggregation_item")).click()
+    await element.all(by.css(".aggregation_item")).first().click();
     expect(getNum()).toBeLessThan(num);
   });
 
